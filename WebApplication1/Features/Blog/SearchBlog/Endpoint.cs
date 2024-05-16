9b8519eb40664ae5ee252fa5731e2edffad7e966
Javi111003 <128992433+Javi111003@users.noreply.github.com>
2024-05-15 20:40:31 -0400

    refs #2 - Adding search a user blog (all posts of an user)

---
+++ WebApplication1/Features/Blog/SearchBlog/Endpoint.cs
@@ -0,0 +1,26 @@
+﻿using Account.Create;
+using MongoDB.Driver.Linq;
+using Posts.Create;
+
+namespace Blog.Search
+{
+    internal sealed class Endpoint : Endpoint<Request, Response, Mapper>
+    {
+        public override void Configure()
+        {
+            Post("/blog/searchblog");
+            AllowAnonymous();
+        }
+
+        public override async Task HandleAsync(Request r, CancellationToken c)
+        {
+            var db = new UsersContext();
+            User user = db.Users.Find(r.Username);
+            db.Entry(user).Collection(u => u.MyBlog).Load();
+            await SendAsync(new Response() { 
+            Message=$"The posts associated to {user.Username} :"
+            ,Blog = user.MyBlog
+            });
+        }
+    }
+}
---
+++ WebApplication1/Features/Blog/SearchBlog/Mapper.cs
@@ -0,0 +1,7 @@
+﻿namespace Blog.Search
+{
+    internal sealed class Mapper : Mapper<Request, Response, object>
+    {
+
+    }
+}
